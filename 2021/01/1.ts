91f34c9d59ee0c5433945f0f0fbf67b5f7a50f40
Henry <mail@henrygressmann.de>
2021-12-01 06:32:14 +0100

    ups
    Signed-off-by: Henry <mail@henrygressmann.de>

--- 2021/01/1.ts
+++ 2021/01/1.ts
@@ -1,17 +1,17 @@
 import { input } from "./input";
 
 const inputs = input.split("\n").map((i) => parseInt(i, 10));
-const measurments = inputs.map((v, i) => {
+const measurements = inputs.map((v, i) => {
   if (i === 0) return NaN;
   const prev = inputs[i - 1];
   return v > prev;
 });
-console.log(`Part 1: ${measurments.filter(Boolean).length}`);
+console.log(`Part 1: ${measurements.filter(Boolean).length}`);
 
-const measurments2 = inputs.map((v, i) => {
+const measurements2 = inputs.map((v, i) => {
   if (i === 0 || i + 3 > inputs.length) return NaN;
   const prev = inputs[i - 1] + v + inputs[i + 1];
   const curr = v + inputs[i + 1] + inputs[i + 2];
   return curr > prev;
 });
-console.log(`Part 2: Sliding Average: ${measurments2.filter(Boolean).length}`);
+console.log(`Part 2: Sliding Average: ${measurements.filter(Boolean).length}`);
